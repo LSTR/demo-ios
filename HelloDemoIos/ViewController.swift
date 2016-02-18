bc9eea9673c41646595b0b3fc1cf22989c4671ef
LesNR <lester.narvasta@jmtech.pe>
2016-02-18 17:07:43 -0500

    calculador e ya funciona

--- HelloDemoIos/ViewController.swift
+++ HelloDemoIos/ViewController.swift
@@ -1,26 +1,90 @@
-//
-//  ViewController.swift
-//  HelloDemoIos
-//
-//  Created by JM Tech on 2/17/16.
-//  Copyright © 2016 JM Tech. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
-//    Hello Comment
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var display : UILabel!
+    
+    var userisenteringnumbers = false
+    
+    @IBAction func enternumber(sender : UIButton){
+        let digit = sender.currentTitle!
+        if userisenteringnumbers{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userisenteringnumbers = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter(){
+        userisenteringnumbers = false
+        operandStack.append(displayValue)
+        print("openStack = \(operandStack)")
     }
-
-
+    
+    var displayValue : Double {
+        get{
+            return NSNumberFormatter ().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userisenteringnumbers = false
+        }
+    }
+    
+    @IBAction func clear(){
+        displayValue = 0
+        display.text = "\(displayValue)"
+        
+    }
+    
+    @IBAction func operate(sender : UIButton){
+        let operation = sender.currentTitle!
+        
+        if(userisenteringnumbers){
+            enter()
+        }
+        
+        switch operation {
+            case "+":performOperation {$0 + $1}
+            case "-":performOperation {$0 - $1}
+            case "X":performOperation {$0 * $1}
+            case "/":performOperation {$0 / $1}
+            default: break
+        }
+    }
+    
+    func performOperation (operation:(Double,Double) -> Double){
+        if(operandStack.count >= 2){
+            displayValue  = operation(operandStack.removeLast(),operandStack.removeLast())
+            
+            enter()
+        }
+    }
+    
+//    func performOperation (operation:Double -> Double){
+//        if(operandStack.count >= 1){
+//            displayValue  = operation(operandStack.removeLast())
+//            
+//            enter()
+//        }
+//    }
+    
+    func add (op1:Double, op2: Double) -> Double{
+        return op1+op2;
+    }
+    
+    func substarct (op1:Double, op2: Double) -> Double{
+        return op1-op2;
+    }
+    
+    func multiply (op1:Double, op2: Double) -> Double{
+        return op1*op2;
+    }
+    
+    func divide (op1:Double, op2: Double) -> Double{
+        return op1/op2;
+    }
 }
